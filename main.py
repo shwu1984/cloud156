d334fcbca336c4ef75d42f8635105edf02c79067
shwu1984 <shwu1984@163.com>
2023-05-21 12:47:16 +0800

    main.py

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-#!/usr/bin/python3
-# -- coding: utf-8 --
-# @Time : 2023/5/19 10:50
-#作者：https://www.52pojie.cn/thread-1231190-1-1.html
-# -------------------------------
-# cron "30 4 * * *" script-path=xxx.py,tag=匹配cron用
-# const $ = new Env('天翼云盘签到');
-
+"""
+[url=https://www.52pojie.cn/thread-1231190-1-1.html]https://www.52pojie.cn/thread-1231190-1-1.html[/url]
+ 
+感谢作者开源天翼云签到部分源码：
+https://github.com/t00t00-crypto/cloud189-action/blob/master/checkin.py 及 [login_function.py](https://github.com/Dawnnnnnn/Cloud189/blob/master/functions/login_function.py)
+"""
 import time
 import re
+# import json
 import base64
 import hashlib
+# from urllib import parse
+ 
 import rsa
 import requests
-import random
-import os
-
-# 变量 ty_username（15605297035）,ty_password（Shwu@1984*）
-ty_username = os.getenv("ty_username")
-ty_password = os.getenv("ty_password")
-# 推送加
-plustoken = os.getenv("plustoken")
-
-
-
+ 
 BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
-
+ 
 B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
+ 
 s = requests.Session()
-
-#推送函数
-def Push(contents):
-    # 推送加
-    headers = {'Content-Type': 'application/json'}
-    json = {"token": plustoken, 'title': '天翼云签到', 'content': contents.replace('\n', '<br>'), "template": "json"}
-    resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
-    print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
-
-
-
+ 
+# 在下面两行的引号内贴上账号（仅支持手机号）和密码
+username = ""
+password = ""
+ 
+_ = """
+if(username == "" or password == ""):
+    username = input("账号：")
+    password = input("密码：")
+# """
+ 
+assert username and password, "在第23、24行填入有效账号和密码"
+ 
+# https://cp.xuthus.cc/ 申请key 并设置测试好酷推
+xuthuskey = ""
+# xuthuskey = "27a...........................7b"
+ 
+if not xuthuskey:
+    print("第36行的xuthuskey 为空，签到结果将不会通过酷推发到你的qq")
+ 
 def int2char(a):
     return BI_RM[a]
-
-
+ 
 def b64tohex(a):
     d = ""
     e = 0
@@ -70,21 +69,19 @@ def b64tohex(a):
     if e == 1:
         d += int2char(c << 2)
     return d
-
-
+ 
+ 
 def rsa_encode(j_rsakey, string):
     rsa_key = f"-----BEGIN PUBLIC KEY-----\n{j_rsakey}\n-----END PUBLIC KEY-----"
     pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(rsa_key.encode())
     result = b64tohex((base64.b64encode(rsa.encrypt(f'{string}'.encode(), pubkey))).decode())
     return result
-
-
+ 
 def calculate_md5_sign(params):
     return hashlib.md5('&'.join(sorted(params.split('&'))).encode('utf-8')).hexdigest()
-
-
-def login(ty_username, ty_password):
-    url = "https://cloud.189.cn/api/portal/loginUrl.action?redirectURL=https://cloud.189.cn/web/redirect.html"
+ 
+def login(username, password):
+    url = "https://cloud.189.cn/udb/udb_login.jsp?pageId=1&redirectURL=/main.action"
     r = s.get(url)
     captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
     lt = re.findall(r'lt = "(.+?)"', r.text)[0]
@@ -92,9 +89,9 @@ def login(ty_username, ty_password):
     paramId = re.findall(r'paramId = "(.+?)"', r.text)[0]
     j_rsakey = re.findall(r'j_rsaKey" value="(\S+)"', r.text, re.M)[0]
     s.headers.update({"lt": lt})
-
-    username = rsa_encode(j_rsakey, ty_username)
-    password = rsa_encode(j_rsakey, ty_password)
+ 
+    username = rsa_encode(j_rsakey, username)
+    password = rsa_encode(j_rsakey, password)
     url = "https://open.e.189.cn/api/logbox/oauth2/loginSubmit.do"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
@@ -112,22 +109,21 @@ def login(ty_username, ty_password):
         "paramId": paramId
     }
     r = s.post(url, data=data, headers=headers, timeout=5)
-    if (r.json()['result'] == 0):
+    if(r.json()['result'] == 0):
         print(r.json()['msg'])
     else:
         print(r.json()['msg'])
     redirect_url = r.json()['toUrl']
     r = s.get(redirect_url)
     return s
-
-
+ 
+ 
 def main():
-    login(ty_username, ty_password)
+    login(username, password)
     rand = str(round(time.time() * 1000))
     surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
     url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
     url2 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN'
-    url3 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
         "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
@@ -136,13 +132,13 @@ def main():
     }
     response = s.get(surl, headers=headers)
     netdiskBonus = response.json()['netdiskBonus']
-    if (response.json()['isSign'] == "false"):
+    if(response.json()['isSign'] == "false"):
         print(f"未签到，签到获得{netdiskBonus}M空间")
         res1 = f"未签到，签到获得{netdiskBonus}M空间"
     else:
         print(f"已经签到过了，签到获得{netdiskBonus}M空间")
         res1 = f"已经签到过了，签到获得{netdiskBonus}M空间"
-
+ 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
         "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
@@ -165,31 +161,23 @@ def main():
         description = response.json()['description']
         print(f"抽奖获得{description}")
         res3 = f"抽奖获得{description}"
-
-    response = s.get(url3, headers=headers)
-    if ("errorCode" in response.text):
-        print(response.text)
-        res3 = ""
-    else:
-        description = response.json()['description']
-        print(f"链接3抽奖获得{description}")
-        res3 = f"链接3抽奖获得{description}"
-
-    message = res1+res2+res3
-    Push(contents=message)
-
+ 
+    if xuthuskey.strip():
+        _ = xuthuskey.strip()
+        requests.get(f"https://push.xuthus.cc/send/{_}?c=func compute: {res1}, {res2}, {res3}")
+ 
+ 
 def lambda_handler(event, context):  # aws default
     main()
-
-
+ 
+ 
 def main_handler(event, context):  # tencent default
     main()
-
-
+ 
+ 
 def handler(event, context):  # aliyun default
     main()
-
-
+ 
+ 
 if __name__ == "__main__":
-    time.sleep(random.randint(2, 6))
     main()
